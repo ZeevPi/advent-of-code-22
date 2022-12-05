ad17b2d64ed09078af8156ded0323cbe17fdbb08
Vladimir Piven <vladimir.piven@adesso.de>
2022-12-05 13:46:02 +0100

    Refactoring of the main class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,19 @@
-import days.*
-
-val DAYS: Map<Int, Day> = mapOf(
-    Pair(1, Day01()),
-    Pair(2, Day02()),
-    Pair(3, Day03()),
-    Pair(4, Day04())
-)
+import days.Day
+import kotlin.reflect.full.primaryConstructor
 
 fun main() {
     println("Hi there! Please enter the number of the day to get the appropriate solution: ")
-    val day = readLine()!!.toInt()
-    if (DAYS.containsKey(day)) {
-        println("Solution of part one: ${DAYS[day]!!.part1()}")
-        println("Solution of part two: ${DAYS[day]!!.part2()}")
-    } else {
-        println("Sorry, there is no solution for the day '$day'")
+    val input = readLine()!!
+    try {
+        val dayNum = input.toInt()
+        val day = Class.forName(String.format("days.Day%02d", dayNum)).kotlin.primaryConstructor!!.call() as Day
+        println("Solution of part one: ${day.part1()}")
+        println("Solution of part two: ${day.part2()}")
+    } catch (e: Exception) {
+        when (e) {
+            is ClassNotFoundException -> println("Sorry, there is no solution for the day '$input'")
+            is NumberFormatException -> println("Sorry, but you have to enter a valid day. '$input' is invalid!")
+            else -> throw e
+        }
     }
 }
